c061637d1b7950b7ab8e3749b80b0bad5dcd8e1a
MoYasser165 <151955108+MoYasser165@users.noreply.github.com>
2023-11-27 20:11:31 +0200

    Update Input.h

--- GUI/Input.h
+++ GUI/Input.h
@@ -15,7 +15,7 @@ public:
 	Input(window *pW);		//Consturctor
 	void GetPointClicked(int &x, int &y) const;//Get coordinate where user clicks
 	string GetSrting(Output* pO) const ;	 //Returns a string entered by the user
-	void getValidDrawPoint(Point& P) const;
+	void getValidDrawPoint(Point& P) const;//check that the point that we click in the drawing area (neither tool nor status bar) 
 	ActionType GetUserAction() const; //Read the user click and map to an action
 
 	~Input();
