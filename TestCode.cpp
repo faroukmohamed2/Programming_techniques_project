d49e68a9d7b59666af303cb3ae5bec9f5bd9d086
MoYasser165 <mohamedelbatesh0@gmail.com>
2023-12-03 15:30:12 +0200

    git hub test

--- TestCode.cpp
+++ TestCode.cpp
@@ -1,7 +1,7 @@
 #include "GUI\Input.h"
 #include "GUI\Output.h"
 #include <cmath>
-//This is a test code to test the Input and Output classes
+//This is a test code to test the Input and Output classe
 
 int main()
 {
